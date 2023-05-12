d7d21fc2d6c36c24872c2e97b83dccfbe32a414b
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-05-12 10:38:59 +0200

    🟢 should not be full when not all cells are filled

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/board.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/board.spec.ts
@@ -23,20 +23,37 @@ describe("Board", () => {
     });
   });
 
-  it("should be full when all cells are filled", () => {
-    const board = new Board();
+  describe("when checking if the board is full", () => {
+    it("should be full when all cells are filled", () => {
+      const board = new Board();
 
-    board.setCell(0, 0, "X");
-    board.setCell(0, 1, "O");
-    board.setCell(0, 2, "X");
-    board.setCell(1, 0, "O");
-    board.setCell(1, 1, "X");
-    board.setCell(1, 2, "O");
-    board.setCell(2, 0, "X");
-    board.setCell(2, 1, "O");
-    board.setCell(2, 2, "X");
+      board.setCell(0, 0, "X");
+      board.setCell(0, 1, "O");
+      board.setCell(0, 2, "X");
+      board.setCell(1, 0, "O");
+      board.setCell(1, 1, "X");
+      board.setCell(1, 2, "O");
+      board.setCell(2, 0, "X");
+      board.setCell(2, 1, "O");
+      board.setCell(2, 2, "X");
 
-    expect(board.isFull()).toBeTruthy();
+      expect(board.isFull()).toBeTruthy();
+    });
+
+    it("should not be full when not all cells are filled", () => {
+      const board = new Board();
+
+      board.setCell(0, 0, "X");
+      board.setCell(0, 1, "O");
+      board.setCell(0, 2, "X");
+      board.setCell(1, 0, "O");
+      board.setCell(1, 1, "X");
+      board.setCell(1, 2, "O");
+      board.setCell(2, 0, "X");
+      board.setCell(2, 1, "O");
+
+      expect(board.isFull()).toBeFalsy();
+    });
   });
 
   it("should not update the cell value when the cell is not empty", () => {
