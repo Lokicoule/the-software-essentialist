0aad8ce692d2ac477b70665fdcf56114364eefaf
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-05-14 20:31:39 +0200

    🟢 validator pattern validation

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/core/validator.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/core/validator.spec.ts
@@ -2,13 +2,15 @@ import { Validator } from "./validator";
 
 describe("Validator", () => {
   class TestValidator extends Validator<
-    { test: string },
+    { value: string },
     { required: string }
   > {
     protected requiredMessage = "Test is required";
+    protected pattern = /^test$/;
+    protected patternMessage = "Test must be equal to test";
   }
 
-  describe("validate", () => {
+  describe("validateRequired", () => {
     it("should return an error if props is empty", () => {
       const validator = new TestValidator();
 
@@ -22,7 +24,27 @@ describe("Validator", () => {
     it("should return an empty object if props is valid", () => {
       const validator = new TestValidator();
 
-      const result = validator.validate({ test: "test" });
+      const result = validator.validate({ value: "test" });
+
+      expect(result).toEqual({});
+    });
+  });
+
+  describe("validatePattern", () => {
+    it("should return an error if props is not valid", () => {
+      const validator = new TestValidator();
+
+      const result = validator.validate({ value: "toto" });
+
+      expect(result).toEqual({
+        pattern: "Test must be equal to test",
+      });
+    });
+
+    it("should return an empty object if props is valid", () => {
+      const validator = new TestValidator();
+
+      const result = validator.validate({ value: "test" });
 
       expect(result).toEqual({});
     });
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/core/validator.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/core/validator.ts
@@ -1,21 +1,45 @@
 interface BaseValidationError {
   required?: string;
+  pattern?: string;
 }
+
+interface BaseProps {
+  value: string;
+}
+
 export abstract class Validator<
-  Props,
+  Props extends BaseProps,
   ValidationError extends BaseValidationError
 > {
   protected abstract requiredMessage: string;
+  protected abstract pattern: RegExp;
+  protected abstract patternMessage: string;
 
   public validate(props: Props): ValidationError {
-    return this.validateRequired(props);
+    let error = this.validateRequired(props);
+
+    if (!error.required) {
+      error = this.validatePattern(props);
+    }
+
+    return error;
   }
 
   private validateRequired(props: Props): ValidationError {
     const error: ValidationError = {} as ValidationError;
 
-    if (!props) {
+    if (!props?.value) {
       error.required = this.requiredMessage;
+    }
+
+    return error;
+  }
+
+  private validatePattern(props: Props): ValidationError {
+    const error: ValidationError = {} as ValidationError;
+
+    if (!this.pattern.test(props.value)) {
+      error.pattern = this.patternMessage;
     }
 
     return error;
